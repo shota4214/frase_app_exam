2a7e079c130a2fecad551d7778ad7d15b03bcd27
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-01 23:24:21 +0900

    newとeditのviewを共通化した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # get '/frasses', to: 'frasses#index'
   # get '/frasses', to: 'frasses#new'
-  resources :frasses
+  resources :frasses do
+    collection do
+      post :confirm
+    end
+  end
 end
