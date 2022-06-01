ab9dde6cd627f46e00159dbce3b5b887dc7424a8
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-01 18:21:13 +0900

    テーブルFrasses削除しテーブルPosts作成

--- db/migrate/20220531141348_create_frasses.rb
+++
@@ -1,8 +0,0 @@
-class CreateFrasses < ActiveRecord::Migration[6.0]
-  def change
-    create_table :frasses do |t|
-      t.string :title
-      t.text :content
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_141348) do
+ActiveRecord::Schema.define(version: 2022_06_01_091835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "frasses", force: :cascade do |t|
     t.string "title"
+    t.text "content"
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.text "content"
   end
 
