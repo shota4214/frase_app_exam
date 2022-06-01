e78c12b83bbae27a7137ea306a6d3d45132a4d98
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-01 13:29:02 +0900

    index画面表示完了

--- app/controllers/frasses_controller.rb
+++ app/controllers/frasses_controller.rb
@@ -1,7 +1,22 @@
 class FrassesController < ApplicationController
+
   def index
+    @frasses = Frasse.all
   end
 
   def new
+    @frasse = Frasse.new
+  end
+
+  def create
+    Frasse.create(params.require(:frasse).permit(:title,:content))
+    redirect_to new_frasse_paht
+  end
+
+
+  private
+
+  def frasse_params
+    params.require(:frasse).permit(:title, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # get '/frasses', to: 'frasses#index'
+  # get '/frasses', to: 'frasses#new'
   resources :frasses
 end
