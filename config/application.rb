9e145406b63a2746340395973fee7bad9ffaff71
shota4214 <seshiruff4214@yahoo.co.jp>
2022-06-02 13:09:05 +0900

    css適用

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,7 @@ Bundler.require(*Rails.groups)
 module FrasseApp
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
-
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
